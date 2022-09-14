f3e38f29fc9fae11a727dbd61f767ca10f2f05ee
Fahim-H <fahimlutful@gmail.com>
2022-09-14 20:35:26 +0600

    fixed countdown issues

--- script.js
+++ script.js
@@ -111,10 +111,11 @@ const start = () => {
   countdownOverlay.style.display = 'flex';
 
   const startCountdown = setInterval(() => {
-    countdownOverlay.innerHTML = '<h1>${count}</h1>';
+    countdownOverlay.innerHTML = `<h1>${count}</h1>`;
 
     // finished timer
     if (count == 0) {
+      countdownOverlay.style.visibility = 'hidden';
       // -------------- START TYPING -----------------
       document.addEventListener('keydown', typeController);
       countdownOverlay.style.display = 'flex';
